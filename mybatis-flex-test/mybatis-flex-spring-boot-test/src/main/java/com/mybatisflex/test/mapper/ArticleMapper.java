68dcff67487d61b843cda75497c33c234ba74a2c
falcon <1769477451@qq.com>
2023-07-29 11:12:32 +0800

    补全测试用例

---
+++ mybatis-flex-test/mybatis-flex-spring-boot-test/src/main/java/com/mybatisflex/test/mapper/ArticleMapper.java
@@ -0,0 +1,7 @@
+package com.mybatisflex.test.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.mybatisflex.test.model.Article;
+
+public interface ArticleMapper  extends BaseMapper<Article> {
+}
